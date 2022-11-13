4ac2058e523b6ca48118790d280dfadfa10f6bcb
Nahidul Islam <nahidul.mee@gmail.com>
2022-11-13 22:39:43 +0600

    comment update

--- 01 Basics/1.31_null_coalescing_operator.php
+++ 01 Basics/1.31_null_coalescing_operator.php
@@ -3,6 +3,7 @@
  * It is used to replace the ternary operation in conjunction with isset() function. 
  * The Null coalescing operator returns its first operand if it exists and is not NULL;
  * otherwise it returns its second operand.
+ 
  */
 
 $default_lat = 10.5;
